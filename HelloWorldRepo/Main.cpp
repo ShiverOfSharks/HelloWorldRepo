23e9456dbe058dcc357ec20a18c09723a8610310
DFields <Nopeman@mail.com>
2022-09-21 16:09:35 -0400

    Beta Build Changelog:
    - Back end coding complete! Input functions, player documentation, and goal-checks are all present.
    - Added more of Dylan's code (startgame function, conversion of board creation from auto to loops)
    - Code organization and consistent design focus

--- HelloWorldRepo/Main.cpp
+++ HelloWorldRepo/Main.cpp
@@ -1,67 +1,266 @@
 // ticTacToe_WireSharks.cpp Runner File
 // By Dorien Fields, Dylan De Muth
-// hi
-
 
 #include <iostream>
-
-
-
-/*
-
-    possible plan
-
-class ticTacToe{
-    private:
-        char[] =[3][3];
-        // player1 and 2 obj? | turn based with no score count
-
-
-};//class ticTacToe
-*/  // Object-Oriented Design idea ends here
 
 // Global Variables
 
 int x = 0; // x axis for playermove
 int y = 0; // y axis for playermove
-std::string axis[3][3]; // Grid
+static std::string board[3][3]; // Grid
+int XWins = 0; // Number of times X won
+int OWins = 0; // Number of times O won
+int Ties = 0; // Number of ties
+int rnd = 0; // Round counter for FIndWinner function
 
-// Functions - 
+// Functions
+
+void playermove(int x, int y, int r) // allocate x, y variables to the same position on the grid to act out a player's move. R variable tells whose turn it is. 
+{
+    std::string temp;
+
+    if (r % 2 == 0) temp = "X";
+    else temp = "O";
+    std::cout << temp << " to location: " << x << " , " << y << "\n";
+
+    board[x][y] = temp;
+}
+
+void FindWinner(int win, int r) // Check straights, diagonals, and rows to find if someone has won, then declare that player as the winner. 
+{
+
+    // X win cases
+
+    if (board[0][0].compare("X") == 0 && board[1][0].compare("X") == 0 && board[2][0].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][1].compare("X") == 0 && board[1][1].compare("X") == 0 && board[2][1].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][2].compare("X") == 0 && board[1][2].compare("X") == 0 && board[2][2].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][0].compare("X") == 0 && board[0][1].compare("X") == 0 && board[0][2].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[1][0].compare("X") == 0 && board[1][1].compare("X") == 0 && board[1][2].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[2][0].compare("X") == 0 && board[2][1].compare("X") == 0 && board[2][2].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][0].compare("X") == 0 && board[1][1].compare("X") == 0 && board[2][2].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[2][0].compare("X") == 0 && board[1][1].compare("X") == 0 && board[0][2].compare("X") == 0)
+    {
+        std::cout << "The winner is X!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 1;
+        XWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    };
+
+
+    // O win cases
+
+    if (board[0][0].compare("O") == 0 && board[1][0].compare("O") == 0 && board[2][0].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][1].compare("O") == 0 && board[1][1].compare("O") == 0 && board[2][1].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][2].compare("O") == 0 && board[1][2].compare("O") == 0 && board[2][2].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][0].compare("O") == 0 && board[0][1].compare("O") == 0 && board[0][2].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[1][0].compare("O") == 0 && board[1][1].compare("O") == 0 && board[1][2].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[2][0].compare("O") == 0 && board[2][1].compare("O") == 0 && board[2][2].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[0][0].compare("O") == 0 && board[1][1].compare("O") == 0 && board[2][2].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else if (board[2][0].compare("O") == 0 && board[1][1].compare("O") == 0 && board[0][2].compare("O") == 0)
+    {
+        std::cout << "The winner is O!" << "\n"; // declare X winner, set winner to 1 to denote X won this match, add to X
+        win = 2;
+        OWins++;
+
+        std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+        std::cout << "Thank you for playing.\n";
+        exit(0);
+
+    }
+    else // Tie case
+    {
+        std::cout << "No winner yet..." << "\n";
+        if (r == 8)
+        {
+            std::cout << "It's a tie..." << "\n";
+
+            Ties = 1;
+            win = 0;
+
+            std::cout << " X Wins: " << XWins << " O Wins: " << OWins << " Ties: " << Ties << "\n";
+            std::cout << "Thank you for playing.\n";
+            exit(0);
+        }
+    }
+
+
+
+
+}
+
+
 
 void clearBoard() //sets all elements to " "
-{                   
+{
 
-    for (int i; i = 0; i++) { //row iterator
-        for (int j; j = 0; j++) //col iterator
-            axis[i][j] = " "; //sets elements to " "
+    for (int i = 0; i <= 2; i++) { //row iterator
+        for (int j = 0; j <= 2; j++) //col iterator
+            board[i][j] = " "; //sets elements to " "
     }//for
 
 }//clearBoard()
 
 
 
-void cout_boardBLANK() {//prints board
+void cout_Board() {//prints board
     //clearBoard();
 
-    int countOuterLoop = 0; //counts the amount of loops per line so ther is no extra " _ " character under the board    
-
-    for (auto& index_Outer : axis) {//forloop iterates through row
-
-        int countInnerLoop = 0; //counts the amount of loops per line so ther is no extra " | " character at the end of the board
-
+    for (int i = 0; i < 3; i++) {//forloop iterates through row
 
         std::cout << "\n"; //creates a space
 
-        for (auto& index_Inner : axis) {//forloop iterates through col
+        for (int j = 0; j < 3; j++) {//forloop iterates through col
 
-            std::cout << index_Outer; //prints element in array
-            if (countInnerLoop <= 1) std::cout << " | "; //lines of the board
-            countInnerLoop++; //increments countInnerLoop
+            std::cout << board[i][j]; //prints element in array
+            if (j <= 1) std::cout << " | "; //lines of the board
 
         }//for inner
 
-        if (countOuterLoop <= 1) std::cout << "\n_________\n";
-        countOuterLoop++;
+        if (i <= 1) std::cout << "\n_________\n";
 
     }//for outer
 
@@ -69,11 +268,52 @@ void cout_boardBLANK() {//prints board
 
 }//void cout_boardBLANK()
 
-void playermove(int x, int y) // allocate x, y variables to the same position on the grid to act out a player's move.
-{
-    axis[x][y];
-    std::cout << "O to location: " << x << " , " << y << "." << "\n";
-}
+
+
+int startGame() {
+    clearBoard(); // Empties all elements to " "    
+
+    cout_Board();
+
+    int winner = 0; // Determines winner. 1 is X win, 2 is O win
+    int rnd = 0;
+
+    for (int round = 0; round <= 10; round++) {
+
+        if (round % 2 == 0) {
+            std::cout << "You are X! Where would you like to move? Input a number for x axis and then y, from 0 to 2: " << "\n"; // Input to playermove variable to mark a spot on the grid.
+        }
+        else
+        {
+            std::cout << "You are O! Where would you like to move? Input a number for x axis and then y, from 0 to 2: " << "\n";
+        }
+
+        std::cin >> x;//populates x
+        std::cin >> y;//populates y
+
+        if (x <= -1 || x >= 3) // Input validation for playermove variables
+        {
+            std::cout << "Please enter a value from 0 to 2, defaulting to 1." << "\n";
+            x = 1;
+        }
+        if (y <= -1 || y >= 3)
+        {
+            std::cout << "Please enter a value from 0 to 2, defaulting to 1." << "\n";
+            y = 1;
+        }
+
+        playermove(x, y, round);
+        cout_Board();
+
+        FindWinner(winner, rnd);
+        rnd++;
+
+    }//for
+
+
+    return winner;
+
+}//startGame(){
 
 
 
@@ -85,54 +325,29 @@ int main() {
     // Game Intialization
     while (true) {
 
-     //Start Game Prompt:
+        //Start Game Prompt:
 
-        std::cout << "Start a TicTacToe Game? (Y/N) "; //Asks user for input to start or exit game
+        std::cout << "Start a TicTacToe Game? (y/n) "; //Asks user for input to start or exit game
         std::cin >> startGame_Input; //input for prompt
 
 
 
-        if (startGame_Input.compare("Yes") == 0 || startGame_Input.compare("Y") == 0) { //start game input handling
+        if (startGame_Input.compare("Yes") == 0 || startGame_Input.compare("y") == 0) { //start game input handling
 
-        
-            cout_boardBLANK();
-            
-            std::cout << "You are X! Where would you like to move? Input a number for x axis and then y, from 0 to 2." << "\n"; // Input to playermove variable to mark a spot on the grid.
+            startGame();
 
-            std::cin >> x;
-            if (x <= -1 || x >= 3) // Input validation for playermove variables
-            {
-                std::cout << "Please enter a value from 0 to 2, defaulting to 1." << "\n";
-                x = 1;
-            }
-            else
-            {
-                std::cin >> y;
-                if (y != 0, y != 1, y != 2)
-                {
-                    std::cout << "Please enter a value from 0 to 2, defaulting to 1." << "\n";
-                    y = 1;
-                }
-            }
-            playermove(x, y);
+        } // start game if yes
 
-        /*To do: Everything else. Deciding ties, victories, opponent moves, etc.*/
-            
-
-        } // End of game if player said "yes"
-
-        else if (startGame_Input.compare("No") == 0 || startGame_Input.compare("N") == 0) 
+        else if (startGame_Input.compare("No") == 0 || startGame_Input.compare("n") == 0)
         {
             std::cout << "\nGoodbye!" << std::endl;
             break;
-        } // End of game if player said "no"/
+        } // End of game if player said "no"
 
-        else 
+        else
         { // Input validation
-            std::cout << "\nInvalid Response, Please Enter Valid Response (Y/N)\n" << std::endl;
+            std::cout << "\nInvalid Response, Please Enter Valid Response (y/n)\n" << std::endl;
         }
-
-
 
     }// Game code ends here.
 
@@ -140,4 +355,4 @@ int main() {
 
     return 0;
 
-}// Code ends here
+}//main
